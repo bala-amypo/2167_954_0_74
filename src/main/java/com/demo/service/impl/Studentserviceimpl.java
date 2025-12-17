7a48d8dfba4d45dcbb6a25ea4647ccd285d104c7
bala-amypo <bala@amypo.in>
2025-12-17 07:59:06 +0000

    User commit

--- src/main/java/com/demo/service/impl/Studentserviceimpl.java
+++ src/main/java/com/demo/service/impl/Studentserviceimpl.java
@@ -1,1 +1,8 @@
-package com.example.demo.serrvice.impl
+package com.example.demo.service.impl;
+import java.util.*;
+import org.springframework.stereotype.service;
+import com.example.demo.entity.student;
+@service
+public class StudentServiceImpl implements StudentService{
+    private final Map<Long,Student>store=new Has
+}
